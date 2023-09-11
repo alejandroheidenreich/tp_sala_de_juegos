803814064676b34f615ad4fab10bb16d54b2952a
Ale Heidenreich <heidenreich.alejandro@gmail.com>
2023-09-11 17:54:56 -0300

    deploy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,9 @@ import { HomeComponent } from './components/home/home.component';
 import { ErrorComponent } from './components/error/error.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: '/login', component: LoginComponent },
-  { path: '/home', component: HomeComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
   { path: '**', component: ErrorComponent }
 ];
 
